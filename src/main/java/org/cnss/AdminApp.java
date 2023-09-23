91c9f5bd689c1697c16ee59604a8c816886e042b
mrImad007 <imadeddinzaoui@gmail.com>
2023-09-23 15:53:43 +0100

    creating the admin , agent and patient apps

---
+++ src/main/java/org/cnss/AdminApp.java
@@ -0,0 +1,11 @@
+package org.cnss;
+
+import javax.swing.*;
+
+public class AdminApp {
+    public static void main() {
+        JOptionPane.showMessageDialog(null,"dashboard");
+
+    }
+
+}
--- src/main/java/org/cnss/Main.java
+++ src/main/java/org/cnss/Main.java
@@ -1,9 +1,24 @@
 package org.cnss;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("nissay zouzou");
+        int option = Integer.parseInt(JOptionPane.showInputDialog(null,"Bienvenu dans votre espace macnss\n" +
+                "1 : Espace Agent \n" +
+                "2 : Espace Patient"));
+
+        switch (option){
+            case 1 : {
+                AgentApp.main();
+                break;
+            }case 2 : {
+                PatientApp.main();
+                break;
+            }case 3 : {
+                AdminApp.main();
+                break;
+            }
+        }
     }
 }
